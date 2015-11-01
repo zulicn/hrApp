b532246f776b3e5f203bac96347d6a3ff8193e50
IrmaBalic <test@tests-MacBook-Pro-4.local>
2015-11-01 23:57:39 +0100

    List of new and accepted members. Accept and promote member is finished, search is done only for firstname. Should be finished: email when user is accepted, ajax, lastname search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,13 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @accepted_users = User.where(is_accepted: true)
+    @new_users = User.where(is_accepted: false)
+    if params[:search]
+      @accepted_users = User.where(is_accepted: true).search(params[:search])
+    else
+      @accepted_users = User.where(is_accepted: true)
+    end
   end
 
   def create
@@ -15,6 +21,22 @@ class UsersController < ApplicationController
       respond_with(@user, location: root_path) and return
     end
     render :new
+  end
+
+  # Accept user
+  def accept
+    user = User.find(params[:id])
+    user.is_accepted = true
+    user.save!
+    redirect_to users_path
+  end
+
+  # Promote user to admin
+  def promote
+    user = User.find(params[:id])
+    user.role = Role.find_by(name: 'Admin')
+    user.save!
+    redirect_to users_path
   end
 
   private
