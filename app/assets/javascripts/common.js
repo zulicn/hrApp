b8034342fe7c9661dc3813fca7bb509eb1be86ac
ssuljic <sadzid.etf@gmail.com>
2015-11-15 15:12:09 +0100

    Removed log

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,5 +1,4 @@
 function showApplicationModal(id, task_id, task_description) {
-  console.log(id)
   $(id + ' #task-description').text(task_description);
   $(id + ' #task_id').attr('value', task_id);
   $(id).modal();
