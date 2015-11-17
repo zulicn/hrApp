8cdeef890577587d3d1d81a104b0416a8953a603
IrmaBalic <test@tests-MacBook-Pro-4.local>
2015-11-17 13:23:33 +0100

    REmoved some files

--- with-opt-dir/gems/pg-0.18.4/ext/siteconf20151113-7416-1coecec.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20151113-7416-a1p3vr"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
