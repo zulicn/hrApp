6e099c5221029f7470f7d8d71d9e8b32f00686e2
Nejra <nejra.zulic@atlantbh.com>
2015-12-06 20:53:44 +0100

    indentation

--- app/controllers/admin/event_attendences_controller.rb
+++ app/controllers/admin/event_attendences_controller.rb
@@ -3,9 +3,9 @@ module Admin
     respond_to :html
 
     def charge_fee
-    	event_attendence = EventAttendence.find(params[:event_attendence_id])
-			event_attendence.update_attribute(:is_charge_paid, true)
-			redirect_to admin_event_charges_logs_path(event_attendence.event)
+      event_attendence = EventAttendence.find(params[:event_attendence_id])
+      event_attendence.update_attribute(:is_charge_paid, true)
+      redirect_to admin_event_charges_logs_path(event_attendence.event)
     end
 
     def commit_attendence
