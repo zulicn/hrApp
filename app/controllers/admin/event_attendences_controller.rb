676b3192401ca8f00961d96841fe41a44071f347
Nejra <nejra.zulic@atlantbh.com>
2015-12-06 20:51:47 +0100

    indentation

--- app/controllers/admin/event_attendences_controller.rb
+++ app/controllers/admin/event_attendences_controller.rb
@@ -1,30 +1,30 @@
 module Admin
   class EventAttendencesController < AdminController
-  	respond_to :html
+    respond_to :html
 
-  	def charge_fee
-  	  event_attendence = EventAttendence.find(params[:event_attendence_id])
-      event_attendence.update_attribute(:is_charge_paid, true)
-  		redirect_to admin_event_charges_logs_path(event_attendence.event)
-  	end
+    def charge_fee
+    	event_attendence = EventAttendence.find(params[:event_attendence_id])
+			event_attendence.update_attribute(:is_charge_paid, true)
+			redirect_to admin_event_charges_logs_path(event_attendence.event)
+    end
 
-  	def commit_attendence
-  		event_attendence = EventAttendence.find(params[:event_attendence_id])
-  		event_attendence.update_attribute(:is_attended, true)
-  		redirect_to admin_event_attendence_logs_path(event_attendence.event)
-  	end
+    def commit_attendence
+      event_attendence = EventAttendence.find(params[:event_attendence_id])
+      event_attendence.update_attribute(:is_attended, true)
+      redirect_to admin_event_attendence_logs_path(event_attendence.event)
+    end
 
-  	def charge_undo
-  		event_attendence = EventAttendence.find(params[:event_attendence_id])
-  		event_attendence.update_attribute(:is_charge_paid, false)
-  		redirect_to admin_event_charges_logs_path(event_attendence.event)
-  	end
+    def charge_undo
+      event_attendence = EventAttendence.find(params[:event_attendence_id])
+      event_attendence.update_attribute(:is_charge_paid, false)
+      redirect_to admin_event_charges_logs_path(event_attendence.event)
+    end
 
-  	def undo_attendence
-  		event_attendence = EventAttendence.find(params[:event_attendence_id])
-  		event_attendence.update_attribute(:is_attended, false)
-  		redirect_to admin_event_attendence_logs_path(event_attendence.event)
-  	end
+    def undo_attendence
+      event_attendence = EventAttendence.find(params[:event_attendence_id])
+      event_attendence.update_attribute(:is_attended, false)
+      redirect_to admin_event_attendence_logs_path(event_attendence.event)
+    end
 
   end
 end
