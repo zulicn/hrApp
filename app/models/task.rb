e4b7e9213fe375fdc0925d3ff4a670a7408c4e82
Kanita Imamovic <kimamovic1@etf.unsa.ba>
2015-12-08 09:54:28 +0100

    validation on creating task

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,15 @@ class Task < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
 
   has_many :user_tasks
+  validate :start_date_before_end_date
 
   def late?
     Time.now > deadline
+  end
+
+  def start_date_before_end_date
+    if deadline_to_apply > deadline
+      errors.add(:deadline_to_apply, "Krajnji datum za prijave treba biti prije krajnjeg datuma za završetak zadatka")
+  	end
   end
 end
