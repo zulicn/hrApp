7b78e8cf631dbb24acca23f9de48309e531643b2
ssuljic <sadzid.etf@gmail.com>
2015-12-27 23:10:04 +0100

    Removed sendgrid credentials from repo

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'] || 'app43418156@heroku.com',
-  :password       => ENV['SENDGRID_PASSWORD'] || '7ocwij3a0710',
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
